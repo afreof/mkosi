4e5d1ae4d212f52609ba899a94d19b6ccec6cd56
Adrian Freihofer <adrian.freihofer@gmail.com>
2019-02-25 22:49:08 +0100

    Call pytest from ci/semaphore.sh

--- ci/semaphore.sh
+++ ci/semaphore.sh
@@ -2,10 +2,15 @@
 
 set -ex
 
+# Build an image
 sudo add-apt-repository --yes ppa:jonathonf/python-3.6
 sudo apt --yes update
-sudo apt --yes install python3.6 debootstrap systemd-container squashfs-tools
+sudo apt --yes install python3.6 python3-pip debootstrap systemd-container squashfs-tools
 
 sudo python3.6 ./mkosi --default ./mkosi.files/mkosi.ubuntu
 
 test -f ubuntu.raw
+
+# Run unit tests
+python3.6 -m pip install -U pytest
+python3.6 -m pytest
